3b50bddc0cb5c0f9801f5f27c7baf673edd4be01
Evgeny Saveliev <e.s.saveliev@gmail.com>
2018-06-12 23:52:51 +0100

    Submodule test 2

--- RBush/RBush.cs
+++ RBush/RBush.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 namespace RBush
 {
+	// Submodule test 2.
 	public partial class RBush<T> : ISpatialDatabase<T>, ISpatialIndex<T> where T : ISpatialData
 	{
 		private const int DefaultMaxEntries = 9;
